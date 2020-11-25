f06012c2ce69d5b7ce4996f4fbd3b942e201e074
Mahmoud AbouElawad <35901393+abouelawad@users.noreply.github.com>
2020-11-25 02:01:01 +0200

    autoloading with a callback function

--- autoload.php
+++ autoload.php
@@ -1,16 +1,23 @@
 <?php
 
-spl_autoload_register();
+// spl_autoload_register();
 
 
+//! THIS WAY is not effective for me 
 // spl_autoload_register("printClassName");
-
-
 // function printClassName($className)
 // {
 //   $explodedClassName = explode("\\", $className);
-  
 //   $classEndName = end($explodedClassName);
 //   print_r($classEndName);
 //   echo '<br />';
 // }
+
+
+//* THE SAME EFFECT OF THE PLAIN "spl_autoload_register"
+spl_autoload_register("getClassName");
+function getClassName($className)
+{
+  $filePath = lcfirst($className) . ".php";
+  require_once $filePath;
+}
